4710a422f8347b4dde3c5113c1e5bfc1c7b171f2
GulfamKIIT <24156013@kiit.ac.in>
2025-12-11 22:40:56 +0530

    init commit

---
+++ lab2/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class evenodd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1;i<=10;i++)
+        {
+            if(n%2==0)
+
+        }
+        System.out.println(n);
+    }
+}
